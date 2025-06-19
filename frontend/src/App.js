4bba115a57e63e0c37d4b920d180c1cab37ad490
Aditya Virmani <avirmani2024@gmail.com>
2025-06-19 15:34:02 -0400

    fix for connecting backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,11 @@ import HeroSection from "./HeroSection";
 import UploadSection from "./UploadSection";
 import SuggestedProducts from "./SuggestedProducts";
 import WhyTrustUs from "./WhyTrustUs";
+import axios from "axios";
+
+// If you have any API calls, update them to use the deployed backend:
+// Example:
+// axios.post("https://profit-model.onrender.com/process", ...)
 
 export default function App() {
   return (
