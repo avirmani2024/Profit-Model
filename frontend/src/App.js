403a7a6c9eecf383774712cc035a5a3299c6e152
Aditya Virmani <avirmani2024@gmail.com>
2025-06-19 15:13:46 -0400

    page not working fix

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import HeroSection from "./HeroSection";
+import UploadSection from "./UploadSection";
+import SuggestedProducts from "./SuggestedProducts";
+import WhyTrustUs from "./WhyTrustUs";
+
+export default function App() {
+  return (
+    <div>
+      <HeroSection />
+      <UploadSection />
+      <SuggestedProducts />
+      <WhyTrustUs />
+    </div>
+  );
+}
