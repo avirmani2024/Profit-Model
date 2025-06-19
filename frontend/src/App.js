72fe8eaa599e54b4b2bcd599b297a1dff8afe194
Aditya Virmani <avirmani2024@gmail.com>
2025-06-19 15:50:38 -0400

    third fixes for netify deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,6 @@ import HeroSection from "./HeroSection";
 import UploadSection from "./UploadSection";
 import SuggestedProducts from "./SuggestedProducts";
 import WhyTrustUs from "./WhyTrustUs";
-import axios from "axios";
-import Lottie from "lottie-react";
-import spreadsheetAnim from "./Animation - 1750309740676.json";
-
-// If you have any API calls, update them to use the deployed backend:
-// Example:
-// axios.post("https://profit-model.onrender.com/process", ...)
 
 export default function App() {
   return (
